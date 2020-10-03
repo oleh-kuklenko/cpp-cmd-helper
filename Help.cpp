299107830e9005aa391f6ad125e18d06d132de20
Oleh Kuklenko <oleg.kklimenok@gmail.com>
2020-10-03 16:43:59 +0300

    Texts

--- Help.cpp
+++ Help.cpp
@@ -3,15 +3,15 @@
 void Help::execute(vector<string> params) const {
 	if (params.size() > 1) {
 		if (params[1] == "ocm") {
-			cout << "  Синтаксис: ocm <opencart_version> <from_module> <new_module_name>" << endl;
+			cout << "  Syntax: ocm <opencart_version> <from_module> <new_module_name>" << endl;
 		} else if (params[1] == "exit") {
-			cout << "  Синтаксис: exit" << endl;
+			cout << "  Syntax: exit" << endl;
 		} else {
-			cout << "  Команда " << params[1] << " не определена" << endl;
+			cout << "  Command " << params[1] << " not defined" << endl;
 		}
 	} else {
-		cout << "  Доступные команды: " << endl;
-		cout << "    ocm - клонирование модулей Opencart" << endl;
-		cout << "    exit - выход из приложения" << endl;
+		cout << "  Allowed commands: " << endl;
+		cout << "    ocm - clone module of opencart" << endl;
+		cout << "    exit - exit application" << endl;
 	}
 }
